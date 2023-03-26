ef48f222becbcb1cdc8852e9015dbe36d8344727
Igal Alkon <igal@anzu.io>
2023-03-26 09:16:27 +0300

    fixed range was not inclusive in hexagon shape.

--- src/plugin.rs
+++ src/plugin.rs
@@ -35,11 +35,11 @@ impl Hexagons for HexMap {
     fn build(&mut self, radius: i32) {
         let n = radius;
 
-        for q in -n..n {
+        for q in -n..=n {
             let r1 = max(-n, -q - n);
             let r2 = min(n, -q + n);
 
-            for r in r1..r2 {
+            for r in r1..=r2 {
                 let rep = String::from(format!("({q},{r})"));
                 self.map.insert(Hex::new(q, r), rep);
             }
